2845139b41039ad99c214eee6fe6154f885e62d5
Patrik Nygren <patriknygren82@gmail.com>
2024-05-03 08:52:54 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+### Structure
+
+`index.html` and `main.js` are files loaded into browser
+json-server is a program downloaded with npm which acts as
+a server and using db.json file as a mock database. You can
+use this template to finish the frontend part of the
+assignment, and when done replace json-server with your
+real working backend server when we have learned how to do
+so. :-)
+
+### How to run
+
+- $ git clone <repo-url>
+- $ cd <repo>
+- $ npm install
+- $ npx json-server db.json (don´t close the terminal!)
+- open index.html in browser
+- Done!
---
+++ main.js
@@ -0,0 +1,32 @@
+// Asynchronous function to create user in server
+async function createUser(name, age) {
+  // Make server POST call to server (we will learn more about this later)
+  const response = await fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      age,
+    }),
+  });
+  const createdUser = await response.json();
+  return createdUser;
+}
+
+// Retrieve form from DOM
+const form = document.querySelector("form");
+
+// Add eventListener to form so that when user press "Save User"
+// button we call the server with form data to save the user
+form.addEventListener("submit", async function (event) {
+  event.preventDefault(); // stop page from reloading!
+  const name = form.querySelector("#name").value;
+  const age = form.querySelector("#age").value;
+  const createdUser = await createUser(name, age); // user with id
+  // 1 TODO: populate list
+});
+
+// 2 TODO: when loading page we should retrieve all existing users
+// and display them in list

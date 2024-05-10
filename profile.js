c235c604c0a6f41b77d75e4b92c87405d9ce0552
Patrik Nygren <patriknygren82@gmail.com>
2024-05-10 11:35:36 +0200

    extended example

--- profile.js
+++ profile.js
@@ -2,4 +2,14 @@ const queryString = window.location.search;
 const params = new URLSearchParams(queryString);
 const id = params.get("id");
 
-// TODO use fetch to get user with id from json-server
+async function getUser(id) {
+  const response = await fetch(`http://localhost:3000/users/${id}`);
+  const user = await response.json();
+  return user;
+}
+
+async function main() {
+  const user = await getUser(id);
+  // TODO: add user to DOM
+}
+main();
